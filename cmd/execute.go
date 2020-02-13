91bb49c768698dd6f915c196bfc2315214da4f1f
Elizabeth Engelman <emengelman@gmail.com>
2020-02-13 12:17:16 -0600

    Move storage Transformer interface to storage pkg

--- cmd/execute.go
+++ cmd/execute.go
@@ -21,6 +21,7 @@ import (
 	"time"
 
 	"github.com/makerdao/vulcanizedb/libraries/shared/constants"
+	"github.com/makerdao/vulcanizedb/libraries/shared/factories/storage"
 	"github.com/makerdao/vulcanizedb/libraries/shared/logs"
 	"github.com/makerdao/vulcanizedb/libraries/shared/transformer"
 	"github.com/makerdao/vulcanizedb/libraries/shared/watcher"
@@ -118,7 +119,7 @@ func executeTransformers() {
 }
 
 type Exporter interface {
-	Export() ([]transformer.EventTransformerInitializer, []transformer.StorageTransformerInitializer, []transformer.ContractTransformerInitializer)
+	Export() ([]transformer.EventTransformerInitializer, []storage.TransformerInitializer, []transformer.ContractTransformerInitializer)
 }
 
 func watchEthEvents(w *watcher.EventWatcher, wg *sync.WaitGroup) {
@@ -139,7 +140,7 @@ func watchEthEvents(w *watcher.EventWatcher, wg *sync.WaitGroup) {
 
 func watchEthStorage(w watcher.IStorageWatcher, wg *sync.WaitGroup) {
 	defer wg.Done()
-	// Execute over the StorageTransformerInitializer set using the storage watcher
+	// Execute over the storage.TransformerInitializer set using the storage watcher
 	LogWithCommand.Info("executing storage transformers")
 	ticker := time.NewTicker(pollingInterval)
 	defer ticker.Stop()
