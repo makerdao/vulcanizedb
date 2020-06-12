274592644097d0c821928db89e23783e345e39fc
Elizabeth Engelman <emengelman@gmail.com>
2020-06-12 14:13:06 -0400

    Add a StatusWriter that appends to the file rather than truncates

--- pkg/file_system/status_writer.go
+++ pkg/file_system/status_writer.go
@@ -25,3 +25,35 @@ func NewStatusWriter(file string, message []byte) StatusWriter {
 func (w StatusWriter) Write() error {
 	return ioutil.WriteFile(w.file, w.message, w.perm)
 }
+
+type StatusAppender struct {
+	file    string
+	message []byte
+	perm    os.FileMode
+	fileFlags int
+}
+
+func NewStatusAppender(file string, message []byte) StatusAppender {
+	return StatusAppender{
+		file:    file,
+		message: message,
+		perm:    0644,
+		fileFlags: os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+	}
+}
+func (a StatusAppender) Write() error {
+	file, openErr := os.OpenFile(a.file, a.fileFlags, a.perm)
+	if openErr != nil {
+		return openErr
+	}
+
+	_, writeErr := file.Write(a.message)
+		if writeErr != nil{
+			closeErr := file.Close()
+			if closeErr != nil {
+				return closeErr
+			}
+			return writeErr
+		}
+	return file.Close()
+}
--- pkg/file_system/status_writer_test.go
+++ pkg/file_system/status_writer_test.go
@@ -1,6 +1,7 @@
 package file_system_test
 
 import (
+	"io/ioutil"
 	"os"
 
 	"github.com/makerdao/vulcanizedb/pkg/file_system"
@@ -12,21 +13,77 @@ var _ = Describe("StatusWriter", func() {
 	var (
 		testFileName = "test-file"
 		testFilePath = "/tmp/" + testFileName
-		testFileContents = []byte("test contents")
+		testFileContents = []byte("test contents\n")
 	)
 	AfterEach(func() {
 		err := os.Remove(testFilePath)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	It("It writes the file with the given file name and content", func() {
-		writer := file_system.NewStatusWriter(testFilePath, testFileContents)
-		err := writer.Write()
+	Describe("StatusWriter", func() {
+		var writer = file_system.NewStatusWriter(testFilePath, testFileContents)
 
-		Expect(err).NotTo(HaveOccurred())
-		info, err := os.Stat(testFilePath)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(info.IsDir()).To(BeFalse())
-		Expect(info.Name()).To(Equal(testFileName))
+		It("It writes the contents to the given file path", func() {
+			err := writer.Write()
+			Expect(err).NotTo(HaveOccurred())
+
+			info, err := os.Stat(testFilePath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(info.IsDir()).To(BeFalse())
+			Expect(info.Name()).To(Equal(testFileName))
+
+			contents, err := ioutil.ReadFile(testFilePath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(contents).To(Equal(testFileContents))
+		})
+
+		It("truncates the file contents", func() {
+			err := writer.Write()
+			Expect(err).NotTo(HaveOccurred())
+
+			newFileContents := []byte("new file contents")
+			writer2 := file_system.NewStatusWriter(testFilePath, newFileContents)
+			err2 := writer2.Write()
+			Expect(err2).NotTo(HaveOccurred())
+
+			contents, err := ioutil.ReadFile(testFilePath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(contents).To(Equal(newFileContents))
+		})
+	})
+
+	Describe("StatusAppender", func() {
+		var writer = file_system.NewStatusAppender(testFilePath, testFileContents)
+
+		It("It writes the contents to the given file path", func() {
+			err := writer.Write()
+			Expect(err).NotTo(HaveOccurred())
+
+			info, err := os.Stat(testFilePath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(info.IsDir()).To(BeFalse())
+			Expect(info.Name()).To(Equal(testFileName))
+
+			contents, err := ioutil.ReadFile(testFilePath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(contents).To(Equal(testFileContents))
+		})
+
+		It("appends the content to the end of the given file", func() {
+			err := writer.Write()
+			Expect(err).NotTo(HaveOccurred())
+
+			newFileContents := []byte("new file contents")
+			writer2 := file_system.NewStatusAppender(testFilePath, newFileContents)
+			err2 := writer2.Write()
+			Expect(err2).NotTo(HaveOccurred())
+
+			contents, err := ioutil.ReadFile(testFilePath)
+			expectedContents := []byte(testFileContents)
+			expectedContents = append(expectedContents, []byte(newFileContents)...)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(contents).To(Equal(expectedContents))
+		})
 	})
 })
