0b941942632dbedcd04fc8c4ddadd193b70f51c4
Elizabeth Engelman <emengelman@gmail.com>
2020-11-20 16:02:32 -0500

    Remove test that exercises viper get method

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -24,33 +24,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/spf13/viper"
 )
-
-var vulcanizeConfig = []byte(`
-[database]
-name = "dbname"
-hostname = "localhost"
-port = 5432
-
-[client]
-ipcPath = "IPCPATH/geth.ipc"
-`)
-
 var _ = Describe("Plugin Config", func() {
-	It("reads the private config using the environment", func() {
-		viper.SetConfigName("config")
-		viper.AddConfigPath("$GOPATH/src/github.com/makerdao/vulcanizedb/environments/")
-		Expect(viper.Get("client.ipcpath")).To(BeNil())
-
-		testConfig := viper.New()
-		testConfig.SetConfigType("toml")
-		err := testConfig.ReadConfig(bytes.NewBuffer(vulcanizeConfig))
-		Expect(err).To(BeNil())
-		Expect(testConfig.Get("database.hostname")).To(Equal("localhost"))
-		Expect(testConfig.Get("database.name")).To(Equal("dbname"))
-		Expect(testConfig.Get("database.port")).To(Equal(int64(5432)))
-		Expect(testConfig.Get("client.ipcpath")).To(Equal("IPCPATH/geth.ipc"))
-	})
-
 	var testSubCommand = "test"
 	var testConfig = []byte(`[exporter]
   home = "github.com/makerdao/vulcanizedb"
